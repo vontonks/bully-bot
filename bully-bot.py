fb4918100d19cb3baf440c99340160e921d6e2f2
vontonks <casey.tonkin@gmail.com>
2022-05-11 00:25:00 +1000

    more tidying up

--- bully-bot.py
+++ bully-bot.py
@@ -11,44 +11,23 @@ logging.basicConfig(level=logging.INFO)
 
 client = discord.Client()
 
-#kindly let me know the bot is logged in
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
+# when a message arrives, do things
 @client.event
 async def on_message(message):
-    command = message.content
-    result = re.search('<@(.*)>', command)
+
+    result = re.search('<@(.*)>', message.content)
     
-# make it so the bot doesn't talk to itself -- maybe not necessary since it responds to the $bully command?
-    if message.author == client.user:
-        return
-
-# this is so I can see what it's up to but I'm sure the logger should be doing that
-    print(message.content)
-
-# dunno how to clean this mess up. 
-# Want to keep the ability to program specific insults for people because why not?
-# maybe it can first run a check to see if the user matches a list of special users
-# then rolls to give them a specific or generic insult
-
-    if message.content.startswith('$bully <@969136945613656094>'):
-        await message.channel.send(random.choice(insults.negative))
-        return
-
-    if message.content.startswith('$bully <@261104394987110400>'):
-        await message.channel.send(random.choice(insults.casey))
-        return
-
-    if message.content.startswith('$bully <@259557632447086592>'):
-       await message.channel.send('<@259557632447086592>'+ ' ' + random.choice(insults.richard) + '.')
-       return
-
-    if message.content.startswith('$bully') or message.content.startswith('$BULLY') or message.content.startswith('$Bully'):
-        print(client.user)
+    if message.content.lower().startswith('$bully'):
         user_id = result.group(1)
         await message.channel.send(f'<@{user_id}>'+ ' ' + random.choice(insults.generic) + '.')
+
+# Want to keep the ability to program specific insults for people because why not?
+# maybe it can first run a check to see if the user matches a list of special users
+# then rolls to decide if it givse them a specific or generic insult
 
 # I hid my auth token like a good boy
 client.run(settings.token)
---
+++ users.py
@@ -0,0 +1,7 @@
+from psutil import users
+
+
+#just dumping known users here for future use
+bot = '<@969136945613656094>'
+casey = '<@261104394987110400>'
+richard = '<@259557632447086592>'
